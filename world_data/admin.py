6ce675b48090f561fc63968a94626ebd7616bf13
Piotr Kamiński <67481570+Laplasjan107@users.noreply.github.com>
2022-01-17 22:06:34 +0100

    Dodano aplikacje na wszystkie data-related tabele. Wczytywanie dla world cities

---
+++ world_data/admin.py
@@ -0,0 +1,133 @@
+from django.contrib import admin
+
+from django.shortcuts import render
+from django.urls import path
+from django import forms
+from django.contrib import messages
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
+from .models import WorldHappiness, WorldCities
+
+
+class CsvImportForm(forms.Form):
+    csv_upload = forms.FileField()
+
+
+class WorldHappinessAdmin(admin.ModelAdmin):
+    list_display = ('country_name',)
+
+    def get_urls(self):
+        urls = super().get_urls()
+        new_urls = [path('upload-csv/', self.upload_csv), ]
+        return new_urls + urls
+
+    def upload_csv(self, request):
+        if request.method == "POST":
+            csv_file = request.FILES["csv_upload"]
+
+            if not csv_file.name.endswith('.csv'):
+                messages.warning(request, 'The wrong file type was uploaded')
+                return HttpResponseRedirect(request.path_info)
+
+            file_data = csv_file.read().decode("utf-8").replace('\r', '0')
+            csv_data = file_data.split("\n")
+            first = True
+            for x in csv_data:
+                if len(x) == 0:
+                    break
+                if first:
+                    first = False
+                    continue
+                fields = x.split(",")
+                fields_with_nulls = []
+                for i in range(11):
+                    if not fields[i]:
+                        fields_with_nulls.append(None)
+                    else:
+                        fields_with_nulls.append(fields[i])
+                created = WorldHappiness.objects.update_or_create(
+                    country_name=fields_with_nulls[0],
+                    year=fields_with_nulls[1],
+                    life_ladder=fields_with_nulls[2],
+                    log_GDP_per_capita=fields_with_nulls[3],
+                    social_support=fields_with_nulls[4],
+                    healthy_life_expectancy=fields_with_nulls[5],
+                    freedom_of_life_choices=fields_with_nulls[6],
+                    generosity=fields_with_nulls[7],
+                    perceptions_of_corruption=fields_with_nulls[8],
+                    positive_affect=fields_with_nulls[9],
+                    negative_affect=fields_with_nulls[10],
+                )
+            url = reverse('admin:index')
+            return HttpResponseRedirect(url)
+
+        form = CsvImportForm()
+        data = {"form": form}
+        return render(request, "admin/csv_upload.html", data)
+
+
+class WorldCitiesAdmin(admin.ModelAdmin):
+    list_display = ('city',)
+
+    def get_urls(self):
+        urls = super().get_urls()
+        new_urls = [path('upload-csv/', self.upload_csv), ]
+        return new_urls + urls
+
+    def upload_csv(self, request):
+        WorldCities.objects.all().delete()
+        if request.method == "POST":
+            csv_file = request.FILES["csv_upload"]
+
+            if not csv_file.name.endswith('.csv'):
+                messages.warning(request, 'The wrong file type was uploaded')
+                return HttpResponseRedirect(request.path_info)
+
+            file_data = csv_file.read().decode("utf-8").replace('\r', '0')
+            csv_data = file_data.split("\n")
+            first = True
+            for x in csv_data:
+                if len(x) == 0:
+                    break
+                if first:
+                    first = False
+                    continue
+                fields = x.split("\",\"")
+                fields_with_nulls = []
+                for i in range(10):
+                    if not fields[i]:
+                        fields_with_nulls.append(None)
+                    else:
+                        fields_with_nulls.append(fields[i].replace("\"", ""))
+
+                try:
+                    created = WorldCities.objects.update_or_create(
+                        city=fields_with_nulls[0],
+                        city_ascii=fields_with_nulls[1],
+                        latitude=float(fields_with_nulls[2]),
+                        longitude=float(fields_with_nulls[3]),
+                        country=fields_with_nulls[4],
+                        iso2=fields_with_nulls[5],
+                        iso3=fields_with_nulls[6],
+                        admin_name=fields_with_nulls[7],
+                        capital=fields_with_nulls[8],
+                        population=fields_with_nulls[9],
+                    )
+                except:
+                    print("falied to create " + fields_with_nulls[0])
+            url = reverse('admin:index')
+            return HttpResponseRedirect(url)
+
+        form = CsvImportForm()
+        data = {"form": form}
+        return render(request, "admin/csv_upload.html", data)
+
+
+#
+#
+
+# Register your models here.
+
+admin.site.register(WorldHappiness, WorldHappinessAdmin)
+admin.site.register(WorldCities, WorldCitiesAdmin)
---
+++ world_data/models.py
@@ -0,0 +1,41 @@
+from django.db import models
+
+
+# Create your models here.
+
+#
+class WorldHappiness(models.Model):
+    country_name = models.CharField(max_length=200)
+    year = models.IntegerField(default=None, blank=True, null=True)
+    life_ladder = models.FloatField(default=None, blank=True, null=True)
+    log_GDP_per_capita = models.FloatField(default=None, blank=True, null=True)
+    social_support = models.FloatField(default=None, blank=True, null=True)
+    healthy_life_expectancy = models.FloatField(default=None, blank=True, null=True)
+    freedom_of_life_choices = models.FloatField(default=None, blank=True, null=True)
+    generosity = models.FloatField(default=None, blank=True, null=True)
+    perceptions_of_corruption = models.FloatField(default=None, blank=True, null=True)
+    positive_affect = models.FloatField(default=None, blank=True, null=True)
+    negative_affect = models.FloatField(default=None, blank=True, null=True)
+
+    def __str__(self):
+        return self.country_name
+
+
+# https://www.kaggle.com/juanmah/world-cities
+# Attribution 4.0 International (CC BY 4.0)
+class WorldCities(models.Model):
+    city = models.CharField(max_length=200)
+    city_ascii = models.CharField(max_length=200, default=None, blank=True, null=True)
+    latitude = models.FloatField(default=None, blank=True, null=True)
+    longitude = models.FloatField(default=None, blank=True, null=True)
+    country = models.CharField(max_length=200, default=None, blank=True, null=True)
+    iso2 = models.CharField(max_length=5, default=None, blank=True, null=True)
+    iso3 = models.CharField(max_length=5, default=None, blank=True, null=True)
+    admin_name = models.CharField(max_length=200, default=None, blank=True, null=True)
+    capital = models.CharField(max_length=200, default=None, blank=True, null=True)
+    population = models.IntegerField(default=None, blank=True, null=True)
+
+    # id = models.IntegerField(default=None, blank=True, null=True)
+
+    def __str__(self):
+        return self.city
