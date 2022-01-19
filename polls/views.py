a82437024acc87236ffebd6c413ac278f053aa34
Jagoda Bracha <jb429153@students.mimuw.edu.pl>
2022-01-19 08:30:15 +0100

    dostep do bazyz views

--- polls/views.py
+++ polls/views.py
@@ -2,7 +2,11 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.template.loader import render_to_string
 
+from world_data.models import WorldHappiness
 from .forms import CityForm
+
+def compute_result(form):
+    pass #tu bedzie obliczanie wyniku dla kazdego miasta
 
 def get_name(request):
     # if this is a POST request we need to process the form data
@@ -13,6 +17,9 @@ def get_name(request):
             name = form.cleaned_data['name']
             languages = form.cleaned_data.get("languages")
             # z jakiegos powodu languages nie wypisuje sie do htmla, ale tutaj w kodzie mamy do niego dostep wiec nie ma problemu
+            countries_list = WorldHappiness.objects.all()
+            # możemy sobie tak pobrać całą kolumnę
+            # print(countries_list)
             rendered = render(request, 'polls/submited.html', {'name': name}, {'languages': languages[0]}) 
             return rendered
 
