45aeb71717eff207c388b3909e57ec9edbf6cddf
Jagoda Bracha <jb429153@students.mimuw.edu.pl>
2022-01-20 14:26:49 +0100

    foreign keys

--- world_data/models.py
+++ world_data/models.py
@@ -11,23 +11,6 @@ from django.db.models.signals import (
 #
 
 
-class WorldHappiness(models.Model):
-    country_iso = models.CharField(max_length=3, blank=False, null=False)
-    year = models.IntegerField(default=None, blank=True, null=True)
-    life_ladder = models.FloatField(default=None, blank=True, null=True)
-    log_GDP_per_capita = models.FloatField(default=None, blank=True, null=True)
-    social_support = models.FloatField(default=None, blank=True, null=True)
-    healthy_life_expectancy = models.FloatField(default=None, blank=True, null=True)
-    freedom_of_life_choices = models.FloatField(default=None, blank=True, null=True)
-    generosity = models.FloatField(default=None, blank=True, null=True)
-    perceptions_of_corruption = models.FloatField(default=None, blank=True, null=True)
-    positive_affect = models.FloatField(default=None, blank=True, null=True)
-    negative_affect = models.FloatField(default=None, blank=True, null=True)
-
-    def __str__(self):
-        return self.country_name
-
-
 # https://www.kaggle.com/juanmah/world-cities
 # Attribution 4.0 International (CC BY 4.0)
 class WorldCities(models.Model):
@@ -43,11 +26,28 @@ class WorldCities(models.Model):
     def __str__(self):
         return self.city + ", " + self.country
 
+class WorldHappiness(models.Model):
+    country_iso = models.ForeignKey(WorldCities, on_delete=models.CASCADE, max_length=3, blank=False, null=False)
+    year = models.IntegerField(default=None, blank=True, null=True)
+    life_ladder = models.FloatField(default=None, blank=True, null=True)
+    log_GDP_per_capita = models.FloatField(default=None, blank=True, null=True)
+    social_support = models.FloatField(default=None, blank=True, null=True)
+    healthy_life_expectancy = models.FloatField(default=None, blank=True, null=True)
+    freedom_of_life_choices = models.FloatField(default=None, blank=True, null=True)
+    generosity = models.FloatField(default=None, blank=True, null=True)
+    perceptions_of_corruption = models.FloatField(default=None, blank=True, null=True)
+    positive_affect = models.FloatField(default=None, blank=True, null=True)
+    negative_affect = models.FloatField(default=None, blank=True, null=True)
+
+    def __str__(self):
+        return self.country_name
+
+
 
 # https://www.kaggle.com/cityapiio/world-cities-average-internet-prices-2020
 # CC0: Public Domain
 class InternetPrices(models.Model):
-    city = models.CharField(max_length=200)
+    city = models.ForeignKey(WorldCities, on_delete=models.CASCADE, max_length=200)
     # TODO: zmiana country na iso3?
     country = models.CharField(max_length=200)
     price_usd = models.FloatField(default=None, blank=True, null=True)
@@ -55,11 +55,23 @@ class InternetPrices(models.Model):
     def __str__(self):
         return self.city
 
+# https://www.kaggle.com/timoboz/country-data?select=continent.json
+class Iso(models.Model):
+    country = models.CharField(max_length=200, default=None, primary_key=True)
+    iso2 = models.CharField(max_length=2, default=None, blank=True, null=True)
+    iso3 = models.CharField(max_length=3, default=None, blank=True, null=True)
+    numeric = models.IntegerField(default=None, blank=True, null=True)
+    iso2_continent = models.CharField(max_length=2, null=True)
+
+    def __str__(self):
+        return self.country
+
+
 
 # https://ourworldindata.org/grapher/main-religion-of-the-country-in
 # Creative Commons BY license.
 class CountryReligion(models.Model):
-    iso3 = models.CharField(max_length=3, null=False, blank=False)
+    iso3 = models.ForeignKey(Iso, on_delete=models.CASCADE, max_length=3, null=False, blank=False)
     year = models.IntegerField(default=None, blank=True, null=True)
     main_religion = models.CharField(max_length=200, null=False, blank=False)
 
@@ -70,7 +82,7 @@ class CountryReligion(models.Model):
 # https://www.kaggle.com/cityapiio/world-cities-air-quality-and-water-polution
 # CC0: Public Domain
 class AirWaterQuality(models.Model):
-    city = models.CharField(max_length=200, null=False, blank=False)
+    city = models.ForeignKey(WorldCities, on_delete=models.CASCADE, max_length=200, null=False, blank=False)
     # TODO: zmiana country na iso3?
     country = models.CharField(max_length=200, null=False, blank=False)
     air_quality = models.FloatField(null=True, blank=True)
@@ -87,16 +99,6 @@ def check_has_any_quality(sender, instance, *args, **kwargs):
         raise Exception("Neither water, nor air quality specified")
 
 
-# https://www.kaggle.com/timoboz/country-data?select=continent.json
-class Iso(models.Model):
-    country = models.CharField(max_length=200, default=None, primary_key=True)
-    iso2 = models.CharField(max_length=2, default=None, blank=True, null=True)
-    iso3 = models.CharField(max_length=3, default=None, blank=True, null=True)
-    numeric = models.IntegerField(default=None, blank=True, null=True)
-    iso2_continent = models.CharField(max_length=2, null=True)
-
-    def __str__(self):
-        return self.country
 
 
 @receiver(pre_save, sender=Iso)
